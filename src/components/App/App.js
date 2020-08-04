fe957f432f277ee2f20434b9e426b19d63592dec
dmytro.kukharuk <Di80977298968>
2020-08-04 17:12:50 +0300

    default project cleared

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.scss';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <h1>I can started</h1>
+    </div>
+  );
+}
+
+export default App;
