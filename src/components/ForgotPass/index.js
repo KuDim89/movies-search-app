20c4af0b2a11755340d4e756d04091634b18927c
dmytro.kukharuk <Di80977298968>
2020-08-14 17:34:31 +0300

    added functionality that determines whether the user has permission to visit the application

---
+++ src/components/ForgotPass/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ForgotPass = () => {
+  return (
+      <h1>Pass</h1>
+  )
+}
+
+export default ForgotPass;
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import styles from "./Login.module.scss"
 import logo from "./../../assets/movie-logo.jpg"
-import {Redirect} from "react-router-dom";
+import {Redirect}  from "react-router-dom";
 import {getData} from "../../utils/api";
 
 const Login = () => {
@@ -10,21 +10,27 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const [siteData, setSiteData] = useState('');
   const [homeRedirect, setHomeRedirect] = useState(false);
+  const [validate, setValidate] = useState(false);
 
   useEffect(() => {
-    getData("siteData").then(setSiteData)
-  },[])
+    getData("siteData").then(setSiteData);
+
+    if (phone.match(/^((8|\+{0,9})[\- ]?)?(\(?\d{3,4}\)?[\- ]?)?[\d\- ]{5,10}$/)
+        && password.length > 6)
+    {
+      setValidate(true)
+    }
+  },[phone, password])
 
   const toRegister = () => {
     setRegisterRedirect(true)
   }
 
   const checkUser = () => {
-    debugger
-   const siteArray = siteData.slice(0).find(user => user.phone === phone && user.password === password)
-   if (siteArray) {
-     setHomeRedirect(true)
-   }
+    const siteArray = siteData.slice(0).find(user => user.phone === phone && user.password === password)
+    if (siteArray) {
+      setHomeRedirect(true)
+    }
   }
 
   return (
@@ -38,42 +44,50 @@ const Login = () => {
                   </div>
                   <h3 className="mb-5 text-center">We are JustWatch</h3>
                   <h6>Please login to your account</h6>
+                  {!validate && phone && password
+                    ? <div className="invalid-feedback d-block text-center">Phone or password invalid</div>
+                    : null
+                  }
                   <form>
                     <div className="form-group">
-                    <label className="form-control-label">Phone</label>
-                    <input
-                      type="text"
-                      name="phone"
-                      placeholder="+385619086171"
-                      className="form-control"
-                      onChange={e => setPhone(e.target.value)}
-                    />
-                  </div>
+                      <label className="form-control-label">Phone</label>
+                      <input
+                        type="text"
+                        name="phone"
+                        placeholder="+385619086171"
+                        className={`form-control ${phone.match(/^((8|\+{0,9})[\- ]?)?(\(?\d{3,4}\)?[\- ]?)?[\d\- ]{12,13}$/) ? "is-valid" : "is-invalid"}`}
+                        onChange={e => setPhone(e.target.value)}
+                        />
+                    </div>
+
 
                     <div className="form-group">
-                    <label className="form-control-label text-muted">Password</label>
-                    <input
-                      type="password"
-                      name="password"
-                      autoComplete="off"
-                      placeholder="Password"
-                      className="form-control"
-                      onChange={e => setPassword(e.target.value)}
-                    />
-                  </div>
+                      <label className="form-control-label text-muted">Password</label>
+                      <input
+                        type="password"
+                        name="password"
+                        autoComplete="off"
+                        placeholder="Password"
+                        className={`form-control ${password.length > 6 ? "is-valid" : "is-invalid"}`}
+                        onChange={e => setPassword(e.target.value)}
+                      />
+                    </div>
 
                     <div className="row justify-content-center my-3 px-3">
                       <button
                         className="btn-secondary btn-block btn-color py-2"
                         onClick={checkUser}
+                        disabled={!validate}
                       >Login to JustWatch</button>
                       {homeRedirect && (
                           <Redirect to='/home'/>
                       )}
                     </div>
                   </form>
-                  <div className="row justify-content-center my-2"><a href="#" onClick={ (event) => event.preventDefault() }><small className="text-muted">Forgot
-                    Password?</small></a>
+                  <div className="row justify-content-center my-2">
+                    <a href={"/forgotPass"}>
+                      <small className="text-muted">Forgot Password?</small>
+                    </a>
                   </div>
                 </div>
               </div>
